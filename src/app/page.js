a8d63bea7e794d42d3a8a5bf7f5038a452bda0ec
sh20raj <shaswatraj3@gmail.com>
2024-10-12 12:55:45 +0530

    error

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function page() {
         <OtherSkills />
       </div>
       <div className="flex flex-col gap-4 m-20">
-        <h1 className="text-4xl font-bold text-center ">Projects <span className="text-primary underline decoration-wavy decoration-blue-500">I've Worked On</span></h1>
+        <h1 className="text-4xl font-bold text-center ">Projects <span className="text-primary underline decoration-wavy decoration-blue-500">I&apos;ve Worked On</span></h1>
         <Projects />
       </div>
       <Footer />
